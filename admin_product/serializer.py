6ccfe787aec391b9e0155ce518ef7a3b3de93717
Faheema Mariyam <faheemamariyam2102004@gmail.com>
2025-12-02 11:00:54 +0530

    abc

--- admin_product/serializer.py
+++ admin_product/serializer.py
@@ -1,20 +1,31 @@
-
 # from rest_framework import serializers
-# from product.models import Product,Category
+# from product.models import Product, Category
 
 # class AdminProductSerializer(serializers.ModelSerializer):
+#     image = serializers.SerializerMethodField()  # add this
+
 #     class Meta:
 #         model = Product
 #         fields = ['id', 'name', 'description', 'price', 'stock', 'category', 'image']
-# #used new serializer instead of product serializer bcz, in product serializer stock=read only,but admin need to change the stock
-# # Now 'category' can accept an ID
+
+#     def get_image(self, obj):
+#         if obj.image:
+#             try:
+#                 return obj.image.url  # CloudinaryField provides .url
+#             except Exception:
+#                 return None
+#         return None
 from rest_framework import serializers
-from product.models import Product, Category
+from product.models import Product
 
 class AdminProductSerializer(serializers.ModelSerializer):
-    # Ensure category is writeable via primary key
-    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'price', 'stock', 'category', 'image']
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url.replace("http://", "https://")  # enforce HTTPS
+        return None
